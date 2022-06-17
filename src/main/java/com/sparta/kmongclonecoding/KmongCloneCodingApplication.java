979012a7aa2be27bd6fdee0cfe15afccff5152c0
WE-DA-pluggg <79959576+WE-DA-pluggg@users.noreply.github.com>
2022-06-17 17:15:17 +0900

    Initial commit

---
+++ src/main/java/com/sparta/kmongclonecoding/KmongCloneCodingApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.kmongclonecoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmongCloneCodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KmongCloneCodingApplication.class, args);
+    }
+
+}
