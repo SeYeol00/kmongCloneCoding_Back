f42d33a7558664fa0ee8068fa546afa85de729e4
minji-kim525 <alswlwkd20@naver.com>
2022-06-18 10:21:21 +0900

    의미없는 커밋

--- src/main/java/com/sparta/kmongclonecoding/controller/ProjectController.java
+++ src/main/java/com/sparta/kmongclonecoding/controller/ProjectController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,11 +25,10 @@ public class ProjectController {
     public List<HomePageResponseDefaultDto> getHomePage(){
         return projectService.getHomePage();
     }*/
-    }
 
     @PostMapping("/projects/project")
-    public void createProject(ProjectRequestDto projectRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails){
+    public void createProject(ProjectRequestDto projectRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         //일단 리턴값 void 로 설정
-        projectService.createProject(projectRequestDto,userDetails.getUser().getId());
+        projectService.createProject(projectRequestDto, userDetails.getUser().getId());
     }
 }
