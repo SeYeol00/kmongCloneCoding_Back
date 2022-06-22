4f034a889869d13a023dca8246570f02088129cf
이동재 <djlesque@gmail.com>
2022-06-22 23:05:45 +0900

    Modify / unable to log-in with not-generated Id issue / fixed3

--- src/main/java/com/sparta/kmongclonecoding/controller/UserController.java
+++ src/main/java/com/sparta/kmongclonecoding/controller/UserController.java
@@ -21,10 +21,8 @@ import javax.servlet.http.HttpServletResponse;
 @CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
     private final UserService userService;
-
     private final GoogleUserService googleUserService;
-
-    private  final KakaoUserService kakaoUserService;
+    private final KakaoUserService kakaoUserService;
 
     //회원가입
     @PostMapping("/api/signup")
@@ -35,22 +33,21 @@ public class UserController {
     @GetMapping("/api/user/google/callback")
     public ResponseDto<GoogleUserResponseDto> googleLogin(@RequestParam String code, HttpServletResponse httpServletResponse) throws JsonProcessingException {
         GoogleUserResponseDto googleUserResponseDto = googleUserService.googleLogin(code);
-        httpServletResponse.addHeader("Authorization",googleUserResponseDto.getToken());
+        httpServletResponse.addHeader("Authorization", googleUserResponseDto.getToken());
         return ResponseDto.<GoogleUserResponseDto>builder()
                 .status(HttpStatus.OK.toString())
                 .message("구글 소셜 로그인 요청")
                 .data(googleUserResponseDto)
                 .build();
     }
+
     //https://accounts.google.com/o/oauth2/v2/auth?client_id=693007930110-5u66i5c992figci0e3oh3n53sm8q6hb7.apps.googleusercontent.com&redirect_uri=http://localhost:8080/api/user/google/callback&response_type=code&scope=email%20profile%20openid&access_type=offline
     @GetMapping("/user/kakao/callback")
-    public ResponseEntity<KakaoUserResponseDto>  kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
+    public ResponseEntity<KakaoUserResponseDto> kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
         KakaoUserResponseDto kakaoUserResponseDto = kakaoUserService.kakaoLogin(code, response);
         return ResponseEntity.ok().body(kakaoUserResponseDto);
     }
     //https://kauth.kakao.com/oauth/authorize?client_id=346b2f15b0bcf829529a506449139680&redirect_uri=http://localhost:8080/user/kakao/callback&response_type=code
-
-
 
 
 }
